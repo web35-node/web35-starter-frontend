1a630de6ced3ca429b5ded6e62e4b051be4f5375
Brian Hague <alphaseinor@gmail.com>
2020-10-23 17:05:24 -0500

    I can't beleive did the whole thing

---
+++ src/components/DogAdd.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const DogAdd = (props) => {
+
+    const initialFormState = {
+        breed: "",
+        imageUrl:""
+    }
+
+    const [dog, setDog] = useState(initialFormState)
+
+    const changeHandler = (e) => {
+        setDog({...dog, [e.target.name]: e.target.value})
+    }
+
+    const addDog = (e) => {
+        e.preventDefault()
+        axios.post(`${props.baseurl}/dogs`, dog)
+            .then(res => {
+                setDog(initialFormState)
+                props.refreshHandler()
+            })
+            .catch(err =>{
+                console.log(err)
+            })
+    }
+
+
+    return (<>
+        <h2> Add Breed </h2>
+        <form
+            onSubmit={addDog}
+        >
+            <label>
+                Breed
+                <input 
+                    type="text"  
+                    name="breed"
+                    value={dog.breed}
+                    onChange={changeHandler}
+                />
+            </label>
+            <label>
+                URL to Image of Breed
+                <input 
+                    type="text"  
+                    name="imageUrl"
+                    value={dog.imageUrl}
+                    onChange={changeHandler}
+                />
+            </label>
+            <button type="submit">Submit Dog</button>
+        </form>
+    </>)
+}
+
+export default DogAdd
---
+++ src/components/DogCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import axios from 'axios'
+
+const DogCard = (props) => {
+    console.log(props)
+
+    const onDelete = (e) => {
+        e.preventDefault()
+        console.log("delete button on ", props.dog.id)
+        axios.delete(`${props.baseurl}/dogs/${props.dog.id}`)
+            .then((res)=> {
+                console.log(res)
+                props.refreshHandler()
+            })
+            .catch((err)=> {
+                console.log(err)
+            })
+    }
+
+
+    return(
+        <>
+            <article>
+                <h3>{props.dog.breed}</h3>
+                {
+                    props.dog.imageUrl != "" ? <img src={props.dog.imageUrl} alt={props.dog.breed} /> : <p>No Image Found</p>
+                }
+                <button
+                    onClick={onDelete}
+                >Delete Dog</button>
+            </article>
+        </>
+
+    )
+}
+
+export default DogCard
